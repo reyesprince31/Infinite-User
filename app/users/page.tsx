663c9df6c1ea202e5d6ea11955094824db0e9a48
reyesprince31 <reyes.prince31@gmail.com>
2024-10-11 21:44:15 +0800

    fetch users using getUsers service

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,75 +1,20 @@
 import { Metadata } from "next";
 import { UserList } from "./_components/user-list";
+import { getUsers } from "@/lib/server/get-user.service";
 
 export const metadata: Metadata = {
   title: "User List | My App",
   description: "View a list of users from our application",
 };
 
-const users_data = {
-  page: 1,
-  per_page: 6,
-  total: 12,
-  total_pages: 2,
-  data: [
-    {
-      id: 1,
-      email: "george.bluth@reqres.in",
-      first_name: "George",
-      last_name: "Bluth",
-      avatar: "https://reqres.in/img/faces/1-image.jpg",
-    },
-    {
-      id: 2,
-      email: "janet.weaver@reqres.in",
-      first_name: "Janet",
-      last_name: "Weaver",
-      avatar: "https://reqres.in/img/faces/2-image.jpg",
-    },
-    {
-      id: 3,
-      email: "emma.wong@reqres.in",
-      first_name: "Emma",
-      last_name: "Wong",
-      avatar: "https://reqres.in/img/faces/3-image.jpg",
-    },
-    {
-      id: 4,
-      email: "eve.holt@reqres.in",
-      first_name: "Eve",
-      last_name: "Holt",
-      avatar: "https://reqres.in/img/faces/4-image.jpg",
-    },
-    {
-      id: 5,
-      email: "charles.morris@reqres.in",
-      first_name: "Charles",
-      last_name: "Morris",
-      avatar: "https://reqres.in/img/faces/5-image.jpg",
-    },
-    {
-      id: 6,
-      email: "tracey.ramos@reqres.in",
-      first_name: "Tracey",
-      last_name: "Ramos",
-      avatar: "https://reqres.in/img/faces/6-image.jpg",
-    },
-  ],
-  support: {
-    url: "https://reqres.in/#support-heading",
-    text: "To keep ReqRes free, contributions towards server costs are appreciated!",
-  },
-};
-
 export default async function UsersPage() {
+  const { users, total_pages } = await getUsers();
+
   try {
     return (
       <div className="container mx-auto px-4 py-8">
         <h1 className="text-3xl font-bold mb-6">Users</h1>
-        <UserList
-          initialUsers={users_data.data}
-          totalPages={users_data.total_pages}
-        />
+        <UserList initialUsers={users} totalPages={total_pages} />
       </div>
     );
   } catch (error) {
